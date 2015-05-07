1a8fe7719c63ab738028a1411814aa7f080e83ed
jaywcjlove <398188662@qq.com>
2015-05-07 16:41:30 +0800

    添加gulp来压缩js

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename')
+    watch = require('gulp-watch')
+
+//默认执行default任务，并执行监听任务
+gulp.task('default', ['watch'], function() { 
+    // gulp.start('styles', 'scripts', 'images');
+});
+
+//watch任务：侦听js文件执行 build 任务
+gulp.task('watch', function () {
+   gulp.watch('src/*.js', ['build']);//侦听js文件执行 build 任务
+});
+
+//build任务：压缩js | js重命名 | 压缩
+gulp.task('build', function() { 
+    gulp.src('./src/iNotify.js')
+        .pipe(uglify())
+        .pipe(rename({
+            extname:'.min.js'
+        }))
+        .pipe(gulp.dest('./build/'));
+});
