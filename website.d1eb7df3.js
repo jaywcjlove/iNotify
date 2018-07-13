8466e863904fe113752fe90c82d2a7047131f9e8
jaywcjlove <398188662@qq.com>
2018-07-13 20:07:20 +0800

    Updates

--- website.d1eb7df3.js
+++ website.d1eb7df3.js
@@ -48276,7 +48276,6 @@ Notify.prototype = {
       this.addTimer();
     } else {
       this.clearTimer();
-      this.title = str;
     }
     return this;
   },
@@ -48593,7 +48592,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51275' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '56666' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
